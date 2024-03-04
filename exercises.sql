953abe9934c32bf5ced497399e26d37e3898c0aa
MarcoFonti <theemporiumg@gmail.com>
2024-03-04 16:05:55 +0100

    add exercises JOIN

--- exercises.sql
+++ exercises.sql
@@ -40,3 +40,14 @@ SELECT ROUND(AVG(vote)) as avarage_grades, exam_id as id_exam FROM `exam_student
 
 --  4. Contare quanti corsi di laurea ci sono per ogni dipartimento
 SELECT COUNT(id) as degree_courses FROM `degrees` GROUP BY department_id;
+
+
+-- ESERCIZIO CON JOIN
+
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+-- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esam
